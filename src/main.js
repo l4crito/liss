a42199a6db5f5ebf7dabf6244e2b2c84027f7271
l4crito <l4crito@gmail.com>
2021-09-29 20:25:46 -0500

    sin chan, se puede mover

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import kaboom from "https://unpkg.com/kaboom@next/dist/kaboom.mjs";
 const FLOOR_HEIGHT = 90;
 const JUMP_FORCE = 800;
 const SPEED = 480;
+const HEALTH = 30;
+const MOVE_SPEED = 200;
 
 // initialize context
 kaboom();
+debug.paused = false;
 
 // load assets
 
@@ -57,17 +60,52 @@ loadSpriteAtlas("sprites/shin.png", {
 
 scene("game", () => {
   let playing = false;
+  let direction = "r";
+  let canChangeSprite = true;
+  let score = 0;
   // define gravity
   gravity(500);
+
+  const healthbar = add([
+    rect(width(), 24),
+    pos(0, 0),
+    color(127, 255, 127),
+    fixed(),
+    layer("ui"),
+    {
+      max: HEALTH,
+      set(hp) {
+        this.width = (width() * hp) / this.max;
+        this.flash = true;
+      },
+    },
+  ]);
+  healthbar.action(() => {
+    if (healthbar.flash) {
+      const t = time() * 10;
+      healthbar.color.r = wave(127, 255, t);
+      healthbar.color.g = wave(127, 255, t + 1);
+      healthbar.color.b = wave(127, 255, t + 2);
+      healthbar.opacity = 1;
+      healthbar.color = rgb(255, 255, 255);
+      healthbar.flash = false;
+    } else {
+      healthbar.opacity = 1;
+      healthbar.color = rgb(127, 255, 127);
+    }
+  });
 
   // add a game object to screen
   const player = add([
     // list of components
     sprite("liss"),
     pos(200, 40),
-    area(),
+    area({ scale: 0.9 }),
     body(),
+    health(HEALTH),
     "liss",
+    origin("center"),
+    scale(1.5),
   ]);
   player.play("jumpD", { loop: true });
   // floor
@@ -85,13 +123,30 @@ scene("game", () => {
 
   function jump() {
     if (player.grounded()) {
-      player.play("jumpR");
+      if (direction == "l") {
+        player.play("jumpL");
+      } else {
+        player.play("jumpR");
+      }
       player.jump(JUMP_FORCE);
     }
   }
 
   // jump when user press space
-  keyPress("space", jump);
+  keyPress("enter", () => (debug.paused = !debug.paused));
+  keyPress(["up", "space", "w"], jump);
+  keyDown(["left", "a"], () => {
+    player.move(-MOVE_SPEED, 0);
+    direction = "l";
+    setSprite("walkL");
+  });
+
+  keyDown(["right", "d"], () => {
+    player.move(MOVE_SPEED, 0);
+    direction = "r";
+    setSprite("walkR");
+  });
+
   mouseClick(jump);
 
   function spawnSheen() {
@@ -113,12 +168,40 @@ scene("game", () => {
   // start spawning trees
   spawnSheen();
 
+  function setSprite(sprite = "walkR") {
+    if (!player.grounded()) {
+      return;
+    }
+    player.play(sprite);
+  }
+
   // lose if player collides with any game obj with tag "tree"
-  player.collides("shin", () => {
-    go("lose",score);
+  player.collides("shin", (shin, position) => {
+    if (position !== "bottom") {
+      player.hurt(5);
+      healthbar.set(player.hp());
+     
+      shake();
+      healthbar.flash = true;
+      if (player.hp() <= 0) {
+        go("lose");
+      }
+      shin.destroy();
+    }
   });
+
+  player.on("ground", (l) => {
+    if (l.is("shin")) {
+      scoreLabel.text = "Culos mandados alv :" + score;
+      score++;
+      jump();
+      destroy(l);
+      addKaboom(player.pos);
+    }
+  });
+
   player.collides("floor", () => {
-    player.play("walkR", { loop: true });
+    // player.stop();
     if (!playing) {
       shake();
       playing = true;
@@ -127,41 +210,35 @@ scene("game", () => {
   });
 
   // keep track of score
-  let score = 0;
 
-  const scoreLabel = add([text(score), pos(24, 24)]);
-
-  // increment score every frame
-  action(() => {
-    if (!playing) {
-      return;
-    }
-    score++;
-    scoreLabel.text = score;
-  });
-  keyPress("scape", () => go("game"));
+  const scoreLabel = add([
+    text("culos mandados alv: 0"),
+    pos(24, 24),
+    scale(0.5),
+  ]);
 });
 
-scene("lose", (score) => {
+scene("lose", () => {
   add([
     sprite("liss", {
       anim: "die",
     }),
     pos(width() / 2, height() / 2 - 80),
-    scale(2),
+    scale(3),
     origin("center"),
   ]);
 
   // display score
   add([
-    text(score),
+    text("perdiste mmv :c"),
     pos(width() / 2, height() / 2 + 80),
-    scale(2),
+    scale(1.5),
     origin("center"),
   ]);
 
   // go back to game with space is pressed
   keyPress("space", () => go("game"));
+
   mouseClick(() => go("game"));
   shake();
 });
